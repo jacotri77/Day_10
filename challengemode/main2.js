5d3a42222396403b84d147bc1378f74e58841ae5
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-14 16:48:23 -0700

    saving work before heading home

---
+++ challengemode/main2.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	var carouselImages = [
+	  {
+	    image_url: 'http://interfacelift.com/wallpaper/Debbfb5b/03966_upondescent_1280x720.jpg',
+	    title: 'Upon Descent',
+	    photographer_id: 'Phil2001'
+	  },
+	  {
+	    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03965_sunsetinthecascades_1280x720.jpg',
+	    title: 'Sunset in the Cascades',
+	    photographer_id: 'smpoutdoors'
+	  },
+	  {
+	    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03958_seljalandsfossiceland_1280x720.jpg',
+	    title: 'Seljalandsfoss, Iceland',
+	    photographer_id: 'Robin Kamp'
+	  },
+	  {
+	    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03950_godsrays_1280x720.jpg',
+	    title: 'Gods Rays',
+	    photographer_id: 'illuminati'
+	  }
+	];
+
+var newImages = ""
+carouselImages.image_url.forEach(function(image){
+newImages += `<ul><li><img>${image_url}</img<h1>${title}</h1></li></ul>` 
+
+
+
+
+})
+
+
+
+	
+})
---
+++ readme.md
@@ -0,0 +1,8 @@
+// CSS Transitions!!
+
+This is the normal mode for this homework.  The first part contains the following files:
+index.html
+style.css
+main.js
+
+These should produce the desired result.
